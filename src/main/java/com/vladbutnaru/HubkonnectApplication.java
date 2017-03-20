f04796484be4725acbc475abed8bc80a6efa33b4
vladutbutnaru <vlad.butnaru@feel-it-services.com>
2017-03-20 17:26:03 +0200

    Initial commit

---
+++ src/main/java/com/vladbutnaru/HubkonnectApplication.java
@@ -0,0 +1,12 @@
+package com.vladbutnaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubkonnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubkonnectApplication.class, args);
+	}
+}
